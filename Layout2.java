0ed6c36b6d2f3ee2f1ca2ee040ed73cff98c5b77
Manish Bhattarai <hello@maneesh.com.np>
2021-12-05 07:36:51 +0545

    Added Border Layout

--- Layout2.java
+++ Layout2.java
@@ -3,7 +3,7 @@ import java.awt.*;
 
 class Layout2 extends JFrame
 {
-    JButton b1 = new JButton("Button 1");
+    JButton b1 = new JButton("Button 1 Enter your name");
     JButton b2 = new JButton("Button 2");
     JButton b3 = new JButton("Button 3");
     JButton b4 = new JButton("Button 4");
@@ -21,7 +21,7 @@ class Layout2 extends JFrame
         c.setLayout(fl);
         c.add(b1);
         c.add(b2);
-        c.add(b3);
+        c.add(b3,0);
         c.add(b4);
         c.add(b5);
         
---
+++ Layout3.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+import java.awt.*;
+
+class Layout3 extends JFrame
+{
+    JButton b1 = new JButton("Header");
+    JButton b2 = new JButton("Footer");
+    JButton b3 = new JButton("Left Column");
+    JButton b4 = new JButton("Center Colum");
+    JButton b5 = new JButton("Right Column");
+
+    Layout3()
+    {
+        setBounds(100,100,700,500);
+        setTitle("Border Layout");
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Container c = this.getContentPane();
+        BorderLayout b = new BorderLayout();
+        c.add(b1,BorderLayout.PAGE_START);
+        c.add(b2,BorderLayout.PAGE_END);
+        c.add(b3,BorderLayout.LINE_START);
+        c.add(b4,BorderLayout.LINE_END);
+        c.add(b5,BorderLayout.CENTER);
+        
+    }
+
+public static void main (String []args)
+{
+    Layout3 l = new Layout3();
+}
+
+}
