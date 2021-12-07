1bbb0ca6de8920016cfc94c9727aadb75f5915df
Manish Bhattarai <hello@maneesh.com.np>
2021-12-07 07:05:33 +0545

    Completed Tabbed Pane

--- Tabbed.java
+++ Tabbed.java
@@ -10,6 +10,15 @@ public class Tabbed extends JFrame{
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JTabbedPane tabbedPane = new JTabbedPane();
         tabbedPane.setBounds(50,50,300,200);
+        JPanel panel1 = new JPanel();
+        JPanel panel2 = new JPanel();
+        JPanel panel3 = new JPanel();
+        panel1.add(new JLabel("First Page"));
+        panel2.add(new JLabel("Second Page"));
+        panel3.add(new JLabel("Third Page"));
+        tabbedPane.add("First",panel1);
+        tabbedPane.add("Second",panel2);
+        tabbedPane.add("Third",panel3);
         jf.add(tabbedPane);
 }
 
